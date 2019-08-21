06cda7c7a4b6b6907abe902b6109753f4937599c
unknown <2152805@labinfo.is.escuelaing.edu.co>
2019-08-21 09:55:37 -0500

    lab terminado

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,7 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        String aux = (args.length>0)?Arrays.toString(args).replace("[","").replace("]","").replace(",",""):"World";
-        System.out.println("Hello "+aux);
+        System.out.print("Hello ");
+        if(args.length>0){
+            for (int i=0;i<args.length;i++){
+            System.out.print(args[i]+" ");
+            }
+        }else{
+            System.out.print("World");
+        }
+
+        System.out.print("!\n");
     }
 }
--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
@@ -7,7 +7,7 @@ public class ShapeMain {
         }
         try {
 
-            RegularShapeType type = RegularShapeType.valueOf(args[0]);
+            RegularShapeType type = RegularShapeType.valueOf(args[0].substring(0, 1).toUpperCase()+args[0].substring(1));
             Shape shape = ShapeFactory.create(type);
             System.out.println(String.format("Successfully created a %s with %s sides.", type, shape.getNumberOfEdges()));
 
