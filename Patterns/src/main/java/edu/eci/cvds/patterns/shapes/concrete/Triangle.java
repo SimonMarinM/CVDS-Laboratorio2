bf52706c273e6cb88351c60f1858c5aa05a1a9a7
Simón Marín <46967217+SimonMarinM@users.noreply.github.com>
2019-08-23 20:58:37 -0500

    documentando figuras

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Triangle.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Triangle.java
@@ -2,6 +2,9 @@ package edu.eci.cvds.patterns.shapes.concrete;
 
 import edu.eci.cvds.patterns.shapes.Shape;
 
+/**
+* Clase triangulo que representa una figura de 3 lados
+*/
 public class Triangle implements Shape {
     @Override
     public int getNumberOfEdges() {
