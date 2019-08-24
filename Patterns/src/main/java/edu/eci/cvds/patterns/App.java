a5cadbee40e088be1f682a46c00e04f24d254a61
Simón Marín <46967217+SimonMarinM@users.noreply.github.com>
2019-08-23 20:51:36 -0500

    documentando clase app

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -1,11 +1,15 @@
 package edu.eci.cvds;
 import java.util.Arrays;
 /**
- * Hello world!
+ * Clase de prueba para impresión
  *
  */
 public class App 
 {
+    /**
+     * Método que imprime un saludo al usuario
+     * @param args nombre del usuario a saludar (en caso de ser nulo existe un saludo por defecto).     
+     */
     public static void main( String[] args )
     {
         System.out.print("Hello ");
@@ -16,7 +20,6 @@ public class App
         }else{
             System.out.print("World");
         }
-
         System.out.print("!\n");
     }
 }
